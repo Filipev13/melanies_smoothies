603b3f160a7109eb9f38eb4f565f87b807960dca
Filipev13 <160439084+Filipev13@users.noreply.github.com>
2025-06-10 18:15:13 +0100

    Update streamlit_app.py
    fix syntax in col selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,7 @@ name_on_order = st.text_input('Name on Smoothie:')
 if name_on_order:
     st.write(f'The name on your smoothie will be {name_on_order}')
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
-# st.dataframe(my_dataframe, use_container_width = True)
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
 
                                                                 
